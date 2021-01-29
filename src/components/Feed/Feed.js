3cf56f45b04fc4d5cdcf66b6db96e20d3784f0a3
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2021-01-29 16:11:40 -0300

    Final Version

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,4 +1,5 @@
-import { Star } from "@material-ui/icons";
+import { Avatar } from "@material-ui/core";
+import { Comment, EmojiEmotions, Favorite, Gif, Image, LocalActivity, Share, Star } from "@material-ui/icons";
 import React from "react";
 import "./Feed.css";
 
@@ -9,6 +10,26 @@ function Feed() {
         <h2>Home</h2>
         <Star className="feedStar" />
       </div>
+      <div className="feedWrite">
+        <Avatar className="feedWriteAvatar" src={"https://avatars.githubusercontent.com/u/77861206?s=460&u=34c77898a2036ccc169a2a4dc86c9d1e5faa1abc&v=4"} />
+        <p><input placeholder="No que esta pensando?" /></p>
+        <div className="feedWriteICons">
+            <Gif />
+            <EmojiEmotions />
+            <Image />
+        </div>
+        <button className="feedWriteButton">Tweet</button>    
+      </div>
+      <div className="feedTimeLine">
+        <Avatar className="feedTimeLineAvatar" src={"https://avatars.githubusercontent.com/u/77861206?s=460&u=34c77898a2036ccc169a2a4dc86c9d1e5faa1abc&v=4"} />
+        <h4>GustavoNoronha <span>@gustavonoronha</span></h4>
+        <p> Estou Pensando em Programação <span>#code</span></p>
+        <div className="feedTimeLineIcons">
+            <Comment />
+            <Favorite />
+            <Share /> 
+        </div>
+      </div>    
 
     </div>
   );
--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -1,4 +1,4 @@
-import { Search } from "@material-ui/icons";
+import { Search, Settings } from "@material-ui/icons";
 import React from "react";
 import "./Widgets.css";
 
@@ -7,7 +7,22 @@ function Widgets() {
     <div className="widgets">
       <p className="widgetsInput"><Search /> <input placeholder="Busque no Twiiter" /></p>
       <div className="widgetsForYou">
-        For you
+        <div className="widgetsForYouTitles">
+          <h4>Trends For you</h4>
+          <Settings className="widgetsForYouTitlesIcon" />
+        </div>
+        <div className="widgetsForYouList">
+          <h6>Trending WorldWide</h6>
+          <h4>#BreakingNews</h4>
+          <div className="widgetsForYouListItem">
+            <h6>Space</h6>
+            <p>Fotos da lua impressionam</p>
+            <p> o mundo</p>
+            <img className="widgetsForYouListItemImg" src={"https://miro.medium.com/max/4000/1*st5zTbZqIrPQ4tCHzMOQyg.jpeg"} />
+        
+          </div>
+          <h6 className="widgetsForYouListDesc">10.200 pessoas gostaram</h6>
+        </div>
       </div>
     </div>
   );
